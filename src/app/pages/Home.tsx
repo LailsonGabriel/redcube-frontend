305662f986ab7b945be2c3286839c264098d81fa
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2025-06-08 16:32:26 -0300

    adicinado url prod

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -47,7 +47,7 @@ interface ApiConfig {
   getGameRankingEndpoint: (id: string) => string;
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://test-rubcube-backend.onrender.com';
 
 // Configuração dos endpoints da API
 const API_CONFIG: ApiConfig = {
